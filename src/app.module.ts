1d5faffecbcd72c95e90a314d021472b216de6c8
FINEP UTFPR <jeanbrandaocarvalho@hotmail.com>
2024-06-18 21:57:36 -0300

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodoModule } from './todo/todo.module';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TodoModule],
+  imports: [TodoModule, TypeOrmModule],
   controllers: [AppController],
   providers: [AppService],
 })
