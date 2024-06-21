628d9cf40cda2a467b1fd3a22551689dbf2e35f3
FINEP UTFPR <jeanbrandaocarvalho@hotmail.com>
2024-06-21 14:43:37 -0300

    user resources added

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,19 +2,30 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TodosModule } from './todos/todos.module';
+import { UserModule } from './user/user.module';
+import { TodosModule } from './todos/todos.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '8812',
-      database: 'test',
-      entities: [],
-      synchronize: true,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('DB_HOST'),
+        port: +configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        entities: [],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
     }),
+    TodosModule,
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
